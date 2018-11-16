f21963c0098c8e60e62eef6b2b322996472934a4
Shames <assamialomarshames@gmail.com>
2018-11-16 16:19:09 +0600

    Write all the function in sortedType.h

--- SortedType.h
+++ SortedType.h
@@ -1,6 +1,37 @@
 #ifndef SORTEDTYPE_H_INCLUDED
 #define SORTEDTYPE_H_INCLUDED
+#include <iostream>
 
+#define MAX_ITEMS 15
+
+using namespace std;
+
+template<class ItemType>
+
+class SortedType
+{
+
+public:
+    SortedType();
+    ~SortedType();
+    bool is_full();
+    int get_length();
+    void make_empty();
+    bool is_emptyy();
+    ItemType get_item(ItemType item, bool& found);
+    bool put_item(ItemType item);
+    bool delete_item(ItemType item);
+    void reset_list();
+    bool has_next_item();
+    ItemType get_next_item();
+    void print_item();
+
+private:
+    int length;
+    ItemType info[MAX_ITEMS];
+    int currentPos;
+
+};
 
 
 #endif // SORTEDTYPE_H_INCLUDED
